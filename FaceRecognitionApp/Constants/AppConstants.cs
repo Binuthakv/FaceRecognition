ada888694cfeef50d54ba470c432739c023b386d
mkprajeen <mkprajeen@gmail.com>
2026-04-22 14:57:36 +0530

    devtunnel

--- FaceRecognitionApp/Constants/AppConstants.cs
+++ FaceRecognitionApp/Constants/AppConstants.cs
@@ -1,3 +1,5 @@
+using static System.Net.WebRequestMethods;
+
 namespace FaceRecognitionApp.Constants;
 
 /// <summary>
@@ -6,18 +8,18 @@ namespace FaceRecognitionApp.Constants;
 public static class AppConstants
 {
     public const string AppName = "FaceRecognitionApp";
-    
+
     // Camera settings
     public const int FrameCaptureIntervalMs = 500;
     public const double JpegQuality = 0.85;
-    
+
     // Face recognition settings
     public const double DefaultConfidenceThreshold = 70.0;
     public const double HighConfidenceThreshold = 85.0;
-    
+
     // File names
     public const string HaarCascadeFileName = "haarcascade_frontalface_default.xml";
-    
+
     // URLs
     public const string HaarCascadeUrl = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml";
 
@@ -27,14 +29,14 @@ public static class AppConstants
     //    Environment.GetEnvironmentVariable("API_BASE_URL")
     //    ?? "https://localhost:7053/";
     public static string ApiBaseUrl =
-        "https://bc3m95tg-7053.inc1.devtunnels.ms";
+        "https://1j45b432-7053.inc1.devtunnels.ms/";
     //public static string ApiBaseUrl =
     //    "https://localhost:7053/";
     public const string ApiHttpClientName = "FaceRecognitionApi";
-    
+
     // Database
     public const string DatabaseFileName = "facerecognition.db3";
-    
+
     // Messages
     public static class Messages
     {
